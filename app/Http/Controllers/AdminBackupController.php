9dac264aa6837d3fc80b95652fcae1bc09ecf055
SitiWeb <70724099+SitiWeb@users.noreply.github.com>
2023-10-27 22:25:48 +0200

    test2

--- app/Http/Controllers/AdminBackupController.php
+++ app/Http/Controllers/AdminBackupController.php
@@ -17,7 +17,7 @@ class AdminBackupController extends Controller
         //$backups = $request->parameter('archives');
         $repo = new RepositoryController();
         $array = $request->input(); // Get all request data
-        var_dump($request);
+        var_dump($array);
         die;
         foreach($array as $data){
             if (!isset($data['archives'])){
